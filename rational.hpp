499d23770f145accf0c89a3817fa9b61668d690d
Isak Jägberg <ijagberg@gmail.com>
2019-02-01 00:14:23 +0100

    Added decimal value constructor (Rational r = 0.5 => r == 1/2)

--- rational.hpp
+++ rational.hpp
@@ -6,22 +6,23 @@
 
 class ReducedRational;
 
-class Rational {
+class Rational
+{
 	friend class ReducedRational;
 
-private:
+  private:
 	long gcd() const;
 
-public:
+  public:
 	long numerator, denominator;
 
 	Rational();
-	Rational(const long& n);
-	Rational(const long& n, const long& d);
-	Rational(const char* str);
-	Rational(const std::string& str);
-	Rational(const Rational& other);
-	Rational(const ReducedRational& other);
+	Rational(const long &n, const long &d);
+	Rational(const double &d);
+	Rational(const char *str);
+	Rational(const std::string &str);
+	Rational(const Rational &other);
+	Rational(const ReducedRational &other);
 	virtual ~Rational();
 
 	double value() const;
@@ -31,63 +32,73 @@ public:
 	bool isReduced() const;
 
 	/*** Operators ***/
-	Rational& operator+=(const Rational& rhs);
-	Rational& operator-=(const Rational& rhs);
-	Rational& operator*=(const Rational& rhs);
-	Rational& operator/=(const Rational& rhs);
-	Rational& operator++();
+	Rational &operator+=(const Rational &rhs);
+	Rational &operator-=(const Rational &rhs);
+	Rational &operator*=(const Rational &rhs);
+	Rational &operator/=(const Rational &rhs);
+	Rational &operator++();
 	Rational operator++(int);
-	Rational& operator--();
+	Rational &operator--();
 	Rational operator--(int);
 
-	Rational& operator=(const Rational& other);
-	
-	bool operator==(const Rational& rhs) const;
-	bool operator!=(const Rational& rhs) const;
-	bool operator<(const Rational& rhs) const;
-	bool operator<=(const Rational& rhs) const;
-	bool operator>(const Rational& rhs) const;
-	bool operator>=(const Rational& rhs) const;
+	Rational &operator=(const Rational &other);
 
-	friend std::ostream& operator<<(std::ostream& os, const Rational& fraction);
+	bool operator==(const Rational &rhs) const;
+	bool operator!=(const Rational &rhs) const;
+	bool operator<(const Rational &rhs) const;
+	bool operator<=(const Rational &rhs) const;
+	bool operator>(const Rational &rhs) const;
+	bool operator>=(const Rational &rhs) const;
+
+	friend std::ostream &operator<<(std::ostream &os, const Rational &fraction);
 	/***/
 };
 
 static const std::regex type_1 = std::regex("-?[0-9]+");
 static const std::regex type_2 = std::regex("-?[0-9]+/-?[0-9]+");
 
-inline Rational operator+(const Rational& lhs, const Rational& rhs) {
+inline Rational operator+(const Rational &lhs, const Rational &rhs)
+{
 	return Rational(lhs) += rhs;
 }
 
-inline Rational operator-(const Rational& lhs, const Rational& rhs) {
+inline Rational operator-(const Rational &lhs, const Rational &rhs)
+{
 	return Rational(lhs) -= rhs;
 }
 
-inline Rational operator*(const Rational& lhs, const Rational& rhs) {
+inline Rational operator*(const Rational &lhs, const Rational &rhs)
+{
 	return Rational(lhs) *= rhs;
 }
 
-inline Rational operator/(const Rational& lhs, const Rational& rhs) {
+inline Rational operator/(const Rational &lhs, const Rational &rhs)
+{
 	return Rational(lhs) /= rhs;
 }
 
-inline Rational operator%(const Rational& lhs, const Rational& rhs) {
-	if (rhs == lhs || rhs+rhs == lhs || rhs-rhs == lhs)
+inline Rational operator%(const Rational &lhs, const Rational &rhs)
+{
+	if (rhs == lhs || rhs + rhs == lhs || rhs - rhs == lhs)
 		return Rational();
 
 	Rational t(rhs);
-	if (rhs < lhs) {
-		while (t+rhs < lhs) {
+	if (rhs < lhs)
+	{
+		while (t + rhs < lhs)
+		{
 			t += rhs;
 		}
-	} else {
-		while (t > lhs) {
+	}
+	else
+	{
+		while (t > lhs)
+		{
 			t -= rhs;
 		}
 	}
 
-	return Rational(lhs-t);
+	return Rational(lhs - t);
 }
 
 #endif
--- test.cpp
+++ test.cpp
@@ -2,13 +2,18 @@
 #include "rational.hpp"
 #include "reduced_rational.hpp"
 
-int main() {
+int main()
+{
+	Rational a(5);
 	Rational r("53015/125");
 	Rational b(678, 1093);
-    Rational c = "53015/120";
-	std::cout << r << "\n";
-	std::cout << b << "\n";
-	std::cout << (r + b).reduce() << "\n";
-    std::cout << c << "\n";
+	Rational c = "53015/120";
+	Rational d = 0.587193874;
+	std::cout << "a: " << a << std::endl;
+	std::cout << "r: " << r << std::endl;
+	std::cout << "b: " << b << std::endl;
+	std::cout << "(r+b).reduce(): " << (r + b).reduce() << std::endl;
+	std::cout << "c: " << c << std::endl;
+	std::cout << "d: " << d << std::endl;
 	return 1;
 }
