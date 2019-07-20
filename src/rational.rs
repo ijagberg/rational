3ba8fdbb82f685d6e7d7f9488279b5d2f355b079
Isak Jägberg <ijagberg@gmail.com>
2019-07-20 16:07:58 +0200

    Remove cpp files, implement Sub trait

--- src/rational.rs
+++ src/rational.rs
@@ -1,13 +1,15 @@
 use std::ops::Add;
 use std::ops::Mul;
+use std::ops::Sub;
 
+#[derive(Copy, Clone, Debug)]
 struct Rational {
-    numerator: u128,
-    denominator: u128,
+    numerator: i128,
+    denominator: i128,
 }
 
 impl Rational {
-    pub fn new(numerator: u128, denominator: u128) -> Self {
+    pub fn new(numerator: i128, denominator: i128) -> Self {
         if denominator == 0 {
             panic!("Denominator can't be 0");
         }
@@ -18,10 +20,6 @@ impl Rational {
         };
         this.reduce();
         this
-    }
-
-    pub fn as_double(&self) -> f64 {
-        (self.numerator as f64) / (self.denominator as f64)
     }
 
     fn reduce(&mut self) {
@@ -42,11 +40,11 @@ impl Mul for Rational {
     }
 }
 
-impl Mul<u128> for Rational {
+impl Mul<i128> for Rational {
     type Output = Self;
 
-    fn mul(self, rhs: u128) -> Self {
-        self * Rational::new(rhs, 1)
+    fn mul(self, rhs: i128) -> Self {
+        Rational::new(self.numerator * rhs, self.denominator)
     }
 }
 
@@ -54,15 +52,36 @@ impl Add for Rational {
     type Output = Self;
 
     fn add(self, rhs: Self) -> Self {
-        let mut right = rhs;
-        let mut left = self;
-        right = right * self.denominator;
-        left = left * rhs.denominator;
-        Rational::new(right.numerator + left.numerator, right.denominator)
+        let denominator = self.denominator * rhs.denominator;
+
+        Rational::new(
+            self.numerator * rhs.denominator + rhs.numerator * self.denominator,
+            denominator,
+        )
     }
 }
 
-fn gcd(a: u128, b: u128) -> u128 {
+impl Sub for Rational {
+    type Output = Self;
+
+    fn sub(self, rhs: Self) -> Self {
+        self + (rhs * -1_i128)
+    }
+}
+
+impl PartialEq for Rational {
+    fn eq(&self, rhs: &Rational) -> bool {
+        self.numerator * rhs.denominator == rhs.numerator * self.denominator
+    }
+}
+
+impl Into<f64> for Rational {
+    fn into(self) -> f64 {
+        (self.numerator as f64) / (self.denominator as f64)
+    }
+}
+
+fn gcd(a: i128, b: i128) -> i128 {
     let mut a = a;
     let mut b = b;
     while b != 0 {
@@ -71,4 +90,37 @@ fn gcd(a: u128, b: u128) -> u128 {
         a = temp;
     }
     a
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_addition() {
+        let left = Rational::new(1, 2);
+        let right = Rational::new(3, 1);
+        assert_eq!(left + right, Rational::new(7, 2));
+    }
+
+    #[test]
+    fn test_subtraction() {
+        let left = Rational::new(4, 3);
+        let right = Rational::new(1, 2);
+        assert_eq!(left - right, Rational::new(5, 6))
+    }
+
+    #[test]
+    fn test_multiplication() {
+        let left = Rational::new(5, 9);
+        let right = Rational::new(10, 31);
+        assert_eq!(left * right, Rational::new(50, 279));
+    }
+
+    #[test]
+    fn test_equality() {
+        let left = Rational::new(4, 8);
+        let right = Rational::new(16, 32);
+        assert_eq!(left, right);
+    }
 }
