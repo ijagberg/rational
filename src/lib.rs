e94e43ae6abd17fe4303cfec2930f481b7d13299
Isak Jägberg <ijagberg@gmail.com>
2021-04-25 22:16:16 +0200

    Add 'pub' modifier to Rational

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 use std::ops::{Add, Mul, Sub};
 
 #[derive(Copy, Clone, Debug)]
-struct Rational {
+pub struct Rational {
     numerator: i128,
     denominator: i128,
 }
@@ -27,10 +27,34 @@ impl Rational {
     }
 }
 
-impl Mul for Rational {
+macro_rules! impl_from {
+    ($type:ty) => {
+        impl From<$type> for Rational {
+            fn from(v: $type) -> Self {
+                Rational::new(v as i128, 1 as i128)
+            }
+        }
+    };
+}
+
+impl_from!(u8);
+impl_from!(u16);
+impl_from!(u32);
+impl_from!(u64);
+impl_from!(i8);
+impl_from!(i16);
+impl_from!(i32);
+impl_from!(i64);
+impl_from!(i128);
+
+impl<T> Mul<T> for Rational
+where
+    Rational: From<T>,
+{
     type Output = Self;
 
-    fn mul(self, rhs: Self) -> Self {
+    fn mul(self, rhs: T) -> Self::Output {
+        let rhs = Rational::from(rhs);
         let numerator = self.numerator * rhs.numerator;
         let denominator = self.denominator * rhs.denominator;
 
@@ -38,18 +62,14 @@ impl Mul for Rational {
     }
 }
 
-impl Mul<i128> for Rational {
+impl<T> Add<T> for Rational
+where
+    Rational: From<T>,
+{
     type Output = Self;
 
-    fn mul(self, rhs: i128) -> Self {
-        Rational::new(self.numerator * rhs, self.denominator)
-    }
-}
-
-impl Add for Rational {
-    type Output = Self;
-
-    fn add(self, rhs: Self) -> Self {
+    fn add(self, rhs: T) -> Self::Output {
+        let rhs = Rational::from(rhs);
         let denominator = self.denominator * rhs.denominator;
 
         Rational::new(
@@ -59,11 +79,16 @@ impl Add for Rational {
     }
 }
 
-impl Sub for Rational {
+impl<T> Sub<T> for Rational
+where
+    Rational: From<T>,
+{
     type Output = Self;
 
-    fn sub(self, rhs: Self) -> Self {
-        self + (rhs * -1_i128)
+    fn sub(self, rhs: T) -> Self::Output {
+        let rhs = Rational::from(rhs);
+        let rhs_neg: Rational = Mul::<i128>::mul(rhs, -1_i128);
+        Add::<Rational>::add(self, rhs_neg)
     }
 }
 
@@ -95,10 +120,12 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_addition() {
+    fn addition_test() {
         let left = Rational::new(1, 2);
-        let right = Rational::new(3, 1);
-        assert_eq!(left + right, Rational::new(7, 2));
+
+        assert_eq!(left + Rational::new(3, 1), Rational::new(7, 2));
+        assert_eq!(left + 10_u8, Rational::new(21, 2));
+        assert_eq!(left + 217_u16, Rational::new(435, 2));
     }
 
     #[test]
