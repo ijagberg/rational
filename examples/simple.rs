0175fafeba71bcb11a785e0426197ad5776b84c3
ijagberg <ijagberg@gmail.com>
2023-09-19 10:12:52 +0200

    add panic for invalid gcd calculation, and docs

--- examples/simple.rs
+++ examples/simple.rs
@@ -4,4 +4,7 @@ fn main() {
     let one_half = Rational::new(1, 2);
 
     println!("{}", one_half); // prints "1/2"
+
+    println!("{:0b}", -6);
+    println!("{}", (-4_i128 | 2_i128).trailing_zeros());
 }
